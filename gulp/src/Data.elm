d0445cb264adceee21bcfa01a85e0d3ee275ee6d
Mason Mackaman <masondeanm@aol.com>
2020-06-16 16:51:37 -0400

    Refactor getMostPopular

--- gulp/src/Data.elm
+++ gulp/src/Data.elm
@@ -4,11 +4,13 @@ module Data exposing
     , Shell
     , Zone
     , categoryToString
+    , getMostPopular
     , shellToString
     , zoneToString
     )
 
 import Dict exposing (Dict)
+import Dict.Any as DA
 import Http as H
 import Json.Decode as D exposing (Decoder)
 import List.Extra as List
@@ -164,7 +166,117 @@ runsDecoder =
             (D.field "link" D.string)
 
 
+getMostPopular : List Run -> ( Category, Zone )
+getMostPopular =
+    onlyFastestShell
+        >> List.foldl
+            (\run acc ->
+                if DA.member run acc then
+                    DA.get run acc
+                        |> Maybe.map
+                            (\n ->
+                                DA.insert run (n + 1) acc
+                            )
+                        |> Maybe.withDefault acc
 
+                else
+                    DA.insert run 1 acc
+            )
+            (DA.empty to2Id)
+        >> DA.toList
+        >> List.foldl
+            (\( run, count ) acc ->
+                let
+                    ( current, currentCount ) =
+                        acc
+                in
+                if count > currentCount then
+                    ( ( run.category, run.zone )
+                    , count
+                    )
+
+                else
+                    acc
+            )
+            ( ( BossOnly, FF ), 0 )
+        >> Tuple.first
+
+
+to2Id : Run -> ( String, String )
+to2Id run =
+    ( categoryToString run.category, zoneToString run.zone )
+
+
+to3Id : Run -> ( String, ( String, String ) )
+to3Id run =
+    ( run.player, to2Id run )
+
+
+onlyFastestShell : List Run -> List Run
+onlyFastestShell =
+    List.foldl
+        (\run acc ->
+            case DA.get run acc of
+                Just { time } ->
+                    if run.time < time then
+                        DA.insert run run acc
+
+                    else
+                        acc
+
+                Nothing ->
+                    DA.insert run run acc
+        )
+        (DA.empty to3Id)
+        >> DA.toList
+        >> List.map Tuple.second
+
+
+
+-- getMostPopular : List String -> ( Category, String )
+-- getMostPopular zones =
+--     data
+--         |> Dict.foldl
+--             (\_ playerData accum ->
+--                 let
+--                     bossOnly =
+--                         gmpGet BossOnly playerData
+--                     fullRun =
+--                         gmpGet FullRun playerData
+--                     stock =
+--                         gmpGet Stock playerData
+--                 in
+--                 gmpMerge stock accum
+--                     |> gmpMerge fullRun
+--                     |> gmpMerge bossOnly
+--             )
+--             A.empty
+--         |> A.toList
+--         |> List.sortBy Tuple.second
+--         |> List.reverse
+--         |> List.head
+--         |> Maybe.map Tuple.first
+--         |> Maybe.withDefault ( FullRun, "FF" )
+-- gmpMerge : RunAccumulator -> RunAccumulator -> RunAccumulator
+-- gmpMerge ra1 ra2 =
+--     A.merge
+--         (\k a result -> A.insert k a result)
+--         (\k a b result -> A.insert k (a + b) result)
+--         (\k b result -> A.insert k b result)
+--         ra1
+--         ra2
+--         A.empty
+-- gmpGet : Category -> PlayerData -> RunAccumulator
+-- gmpGet category =
+--     getRuns category
+--         >> Dict.toList
+--         >> List.map
+--             (\( zone, _ ) ->
+--                 ( ( category, zone ), 1 )
+--             )
+--         >> A.fromList
+-- type alias RunAccumulator =
+--     A.Dict ( Category, String ) Int
 -- getPlayerData : String -> Maybe PlayerData
 -- getPlayerData player =
 --     Dict.get player data
@@ -241,50 +353,6 @@ runsDecoder =
 --                 Stock ->
 --                     .stock
 --            )
--- getMostPopular : List String -> ( Category, String )
--- getMostPopular zones =
---     data
---         |> Dict.foldl
---             (\_ playerData accum ->
---                 let
---                     bossOnly =
---                         gmpGet BossOnly playerData
---                     fullRun =
---                         gmpGet FullRun playerData
---                     stock =
---                         gmpGet Stock playerData
---                 in
---                 gmpMerge stock accum
---                     |> gmpMerge fullRun
---                     |> gmpMerge bossOnly
---             )
---             A.empty
---         |> A.toList
---         |> List.sortBy Tuple.second
---         |> List.reverse
---         |> List.head
---         |> Maybe.map Tuple.first
---         |> Maybe.withDefault ( FullRun, "FF" )
--- gmpMerge : RunAccumulator -> RunAccumulator -> RunAccumulator
--- gmpMerge ra1 ra2 =
---     A.merge
---         (\k a result -> A.insert k a result)
---         (\k a b result -> A.insert k (a + b) result)
---         (\k b result -> A.insert k b result)
---         ra1
---         ra2
---         A.empty
--- gmpGet : Category -> PlayerData -> RunAccumulator
--- gmpGet category =
---     getRuns category
---         >> Dict.toList
---         >> List.map
---             (\( zone, _ ) ->
---                 ( ( category, zone ), 1 )
---             )
---         >> A.fromList
--- type alias RunAccumulator =
---     A.Dict ( Category, String ) Int
 -- shellToPicture : Shell -> String
 -- shellToPicture shell =
 --     "images/"
